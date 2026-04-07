3f3a748cbb82dae49af815c3094e35b8b94daf0f
Daniel Rios <danieljrm100873@gmail.com>
2026-04-07 04:48:11 +0000

    Finished  Mini02,  implemented the ListIterator edits and safe removal

--- week5-exercises/mini02-list-mechanics-listiterator/src/main/java/edu/norcocollege/cis18b/week5/mini02/AgendaEditor.java
+++ week5-exercises/mini02-list-mechanics-listiterator/src/main/java/edu/norcocollege/cis18b/week5/mini02/AgendaEditor.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-public class AgendaEditor {
+/*
+ * Demonstrates safe list editing with ListIterator by updating,
+ * inserting, removing, and traversing in reverse
+*/
+public class AgendaEditor 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         List<String> agenda = new ArrayList<>(List.of("Intro", "Demo", "Optional", "Quiz"));
 
         applyEdits(agenda);
@@ -15,26 +21,43 @@ public class AgendaEditor {
         printReverse(agenda);
     }
 
-    static void applyEdits(List<String> agenda) {
+    /*
+     * Walks through the agenda using a ListIterator so edits can be made safely.
+     * Replaces "Demo" with "Live Demo", inserts "Reflection" after it,
+     * and removes any item that's labeled "Optional"
+    */
+    static void applyEdits(List<String> agenda) 
+    {
         ListIterator<String> iterator = agenda.listIterator();
 
-        while (iterator.hasNext()) {
+        while (iterator.hasNext()) 
+            {
             String step = iterator.next();
 
-            if ("Demo".equals(step)) {
+            if ("Demo".equals(step)) 
+            {
                 iterator.set("Live Demo");
                 iterator.add("Reflection");
             }
 
-            // TODO: Remove any item labeled "Optional" using iterator-safe mutation.
+            // Safely removes "Optional" while iterating
+            if ("Optional".equals(step)) 
+            {
+                iterator.remove();
+            }
         }
     }
 
-    static void printReverse(List<String> agenda) {
+    /*
+     * Traverses the list backward using ListIterator and prints the reversed order
+    */
+    static void printReverse(List<String> agenda) 
+    {
         ListIterator<String> reverse = agenda.listIterator(agenda.size());
         List<String> reversed = new ArrayList<>();
 
-        while (reverse.hasPrevious()) {
+        while (reverse.hasPrevious()) 
+        {
             reversed.add(reverse.previous());
         }
 
